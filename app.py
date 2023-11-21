05a38e63553d7022eb385c5c90563e24a76e0db0
Swayam Prasad Sah <109921815+swayamps@users.noreply.github.com>
2023-11-22 00:23:37 +0530

    dropdown

--- app.py
+++ app.py
@@ -29,82 +29,75 @@ new_df = df.loc[(df["Age"] < age_upper_limit) & (df["BPM"] < BPM_upper_limit) & 
 # Display the filtered dataframe
 st.write(new_df)
 
-# # Plotting using Streamlit
-# st.subheader("Boxplot of Age")
-# st.boxplot(new_df['Age'])
-# st.subheader("Boxplot of BPM")
-# st.boxplot(new_df['BPM'])
-# st.subheader("Boxplot of Hours per day")
-# st.boxplot(new_df['Hours per day'])
-
-# # Heatmap
-# st.subheader("Correlation Heatmap")
-# numeric_columns = new_df.select_dtypes(include=[np.number]).columns
-# correlation_matrix = new_df[numeric_columns].corr()
-# fig, ax = plt.subplots(figsize=(10, 8))
-# sns.heatmap(correlation_matrix, annot=True, cbar=True, cmap='Blues', ax=ax)
-# st.pyplot(fig)
+# Selectbox for choosing the graph
+graph_choice = st.selectbox('Select a graph', ['Countplot of Primary streaming service', 'Effect of music on mental health', 'Favorite Genre', 'Mental Health vs. Favorite Genre', 'Favorite Genre vs. Age with Music Effects', 'Multiple Barplots'])
 
 # Countplot
-st.subheader("Countplot of Primary streaming service")
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.countplot(x=new_df['Primary streaming service'], ax=ax)
-plt.xticks(rotation=45)
-st.pyplot(fig)
+if graph_choice == 'Countplot of Primary streaming service':
+    st.subheader("Countplot of Primary streaming service")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.countplot(x=new_df['Primary streaming service'], ax=ax)
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
 
 # Pie chart
-st.subheader("Effect of music on mental health")
-effects = new_df['Music effects'].value_counts()
-fig, ax = plt.subplots()
-effects.plot(kind='pie', ylabel='', ax=ax)
-plt.title("Effect of music on mental health")
-st.pyplot(fig)
+elif graph_choice == 'Effect of music on mental health':
+    st.subheader("Effect of music on mental health")
+    effects = new_df['Music effects'].value_counts()
+    fig, ax = plt.subplots()
+    effects.plot(kind='pie', ylabel='', ax=ax)
+    plt.title("Effect of music on mental health")
+    st.pyplot(fig)
 
 # Bar plot
-st.subheader("Favorite Genre")
-y = new_df['Fav genre'].value_counts()
-fig, ax = plt.subplots(figsize=(10, 6))
-y.plot(kind='bar', ax=ax)
-plt.xticks(rotation=90)
-st.pyplot(fig)
+elif graph_choice == 'Favorite Genre':
+    st.subheader("Favorite Genre")
+    y = new_df['Fav genre'].value_counts()
+    fig, ax = plt.subplots(figsize=(10, 6))
+    y.plot(kind='bar', ax=ax)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
 # Line plots
-st.subheader("Mental Health vs. Favorite Genre")
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(x=new_df['Fav genre'], y=new_df['Insomnia'], ci=None, ax=ax)
-plt.xticks(rotation=90)
-st.pyplot(fig)
+elif graph_choice == 'Mental Health vs. Favorite Genre':
+    st.subheader("Mental Health vs. Favorite Genre")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=new_df['Fav genre'], y=new_df['Insomnia'], ci=None, ax=ax)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(x=new_df['Fav genre'], y=new_df['OCD'], ci=None, ax=ax)
-plt.xticks(rotation=90)
-st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=new_df['Fav genre'], y=new_df['OCD'], ci=None, ax=ax)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(x=new_df['Fav genre'], y=new_df['Depression'], ci=None, ax=ax)
-plt.xticks(rotation=90)
-st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=new_df['Fav genre'], y=new_df['Depression'], ci=None, ax=ax)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.lineplot(x=new_df['Fav genre'], y=new_df['Anxiety'], ci=None, ax=ax)
-plt.xticks(rotation=90)
-st.pyplot(fig)
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.lineplot(x=new_df['Fav genre'], y=new_df['Anxiety'], ci=None, ax=ax)
+    plt.xticks(rotation=90)
+    st.pyplot(fig)
 
 # Bar plot with hue
-st.subheader("Favorite Genre vs. Age with Music Effects")
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(data=new_df, x="Fav genre", y="Age", hue="Music effects", errwidth=0, ax=ax)
-plt.xticks(rotation=67)
-st.pyplot(fig)
+elif graph_choice == 'Favorite Genre vs. Age with Music Effects':
+    st.subheader("Favorite Genre vs. Age with Music Effects")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=new_df, x="Fav genre", y="Age", hue="Music effects", errwidth=0, ax=ax)
+    plt.xticks(rotation=67)
+    st.pyplot(fig)
 
 # Subplots
-st.subheader("Multiple Barplots")
-figure, axes = plt.subplots(3, 2, figsize=(18, 10))
-sns.barplot(ax=axes[0, 0], x=new_df['Fav genre'], y=new_df['Age'], hue=new_df['Music effects'], palette="rainbow")
-sns.barplot(ax=axes[0, 1], x=new_df['Fav genre'], y=new_df['Hours per day'], hue=new_df['Music effects'], palette="rainbow")
-sns.barplot(ax=axes[1, 0], x=new_df['Fav genre'], y=new_df['Insomnia'], hue=new_df['Music effects'], palette="rainbow")
-sns.barplot(ax=axes[1, 1], x=new_df['Fav genre'], y=new_df['OCD'], hue=new_df['Music effects'], palette="rainbow")
-sns.barplot(ax=axes[2, 0], x=new_df['Fav genre'], y=new_df['Depression'], hue=new_df['Music effects'], palette="rainbow")
-sns.barplot(ax=axes[2, 1], x=new_df['Fav genre'], y=new_df['Anxiety'], hue=new_df['Music effects'], palette="rainbow")
-plt.tight_layout()
-st.pyplot(figure)
+elif graph_choice == 'Multiple Barplots':
+    st.subheader("Multiple Barplots")
+    figure, axes = plt.subplots(3, 2, figsize=(18, 10))
+    sns.barplot(ax=axes[0, 0], x=new_df['Fav genre'], y=new_df['Age'], hue=new_df['Music effects'], palette="rainbow")
+    sns.barplot(ax=axes[0, 1], x=new_df['Fav genre'], y=new_df['Hours per day'], hue=new_df['Music effects'], palette="rainbow")
+    sns.barplot(ax=axes[1, 0], x=new_df['Fav genre'], y=new_df['Insomnia'], hue=new_df['Music effects'], palette="rainbow")
+    sns.barplot(ax=axes[1, 1], x=new_df['Fav genre'], y=new_df['OCD'], hue=new_df['Music effects'], palette="rainbow")
+    sns.barplot(ax=axes[2, 0], x=new_df['Fav genre'], y=new_df['Depression'], hue=new_df['Music effects'], palette="rainbow")
+    sns.barplot(ax=axes[2, 1], x=new_df['Fav genre'], y=new_df['Anxiety'], hue=new_df['Music effects'], palette="rainbow")
+    plt.tight_layout()
+    st.pyplot(figure)
